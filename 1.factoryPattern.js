0ed670b0635c16e57dc8b631381ff560523fc061
Kalidass <kalidass.k021@gmail.com>
2025-08-13 12:02:37 +0530

    factory pattern

---
+++ 1.factoryPattern.js
@@ -0,0 +1,18 @@
+// factory pattern
+
+// a function that creates different types of objects based on input
+
+const createuser = (name, role) => {
+  const base = { name };
+
+  const roles = {
+    admin: () => ({role: 'Admin', permissions: 'all'}),
+    guest: () => ({role: 'Guest', permissions: 'read-only'}),
+  }
+
+//   return {...base, ...(roles[role]?.() || {})}
+  return {...base, ...(roles[role] && roles[role]?.())}
+};
+
+
+console.log(createuser('Kalidass', 'admin'));
