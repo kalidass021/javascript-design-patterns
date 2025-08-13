8309e8e858b082f17055a318c02e412a1332355a
Kalidass <kalidass.k021@gmail.com>
2025-08-13 11:28:30 +0530

    initial commit

---
+++ 0.designPatterns.js
@@ -0,0 +1,26 @@
+/* 
+    * Design patterns are tried and tested solutions to common software design problems.
+    * They help us to write cleaner, more maintainable, and scalable code by providing reusable templates for solving specific challenges.
+*/
+
+/* 
+    * Types of design patterns
+        
+        1. Creational patterns
+            1. Singleton
+            2. Factory Method
+            3. Abstract Factory
+            4. Builder
+            5. Prototype
+
+        2. Structural Patterns
+            1. Adapter
+            2. Decorator
+            3. Facade
+            4. Proxy
+
+        3. Behavioral Patterns
+            1. Observer
+            2. Iterator
+            3. Chain of Responsibility
+*/
